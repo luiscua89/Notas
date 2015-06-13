a15d62097f3d0756a5b349522fca176109edeeb3
Luis Cua Catzin <luiscuacatzin@Luiss-Mac.local>
2015-06-13 06:52:10 -0700

    Initial Commit

---
+++ MisNotas/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MisNotas
+//
+//  Created by Luis Cua Catzin on 13/06/15.
+//  Copyright (c) 2015 Luis Cua Catzin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
